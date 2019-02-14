72dc62d98e292d16565a0bb14a4bab54cbc417b5
Naman Bhalla <namanbhalla1998@gmail.com>
2019-02-14 22:55:56 +0530

    O(1) space Stack for Integer type

---
+++ py_min_stack/stack_integer.py
@@ -0,0 +1,42 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+        self.current_minimum = float('inf')
+
+    def push(self, item):
+        if not self.stack:
+            self.stack.append(item)
+            self.current_minimum = item
+        else:
+            if item >= self.current_minimum:
+                self.stack.append(item)
+            else:
+                self.stack.append(2 * item - self.current_minimum)
+                self.current_minimum = item
+
+    def pop(self):
+        if not self.stack:
+            raise IndexError
+        else:
+            item = self.stack.pop()
+            if item >= self.current_minimum:
+                return item
+            else:
+                answer = self.current_minimum
+                self.current_minimum = 2 * self.current_minimum - item
+                return answer
+
+    def peek(self):
+        if not self.stack:
+            raise IndexError
+        else:
+            item = self.stack[-1]
+            if item >= self.current_minimum:
+                return item
+            else:
+                return self.current_minimum
+
+    def find_min(self):
+        if not stack:
+            return IndexError
+        return self.current_minimum
